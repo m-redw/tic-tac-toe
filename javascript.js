da045481c32266d55252dc501b836dc1899d3fc4
Edwin <264506916+m-redw@users.noreply.github.com>
2026-06-07 15:16:02 -0700

    Make game winnable

--- javascript.js
+++ javascript.js
@@ -36,7 +36,7 @@ const gameboard = (function() {
         return niceBoard;
     };
 
-    const getCell = (x, y) => gameboard[x-1][y-1];
+    const getCell = (x, y) => board[x-1][y-1].getValue();
 
     const fillCell = (x, y, marker) => {
         const cell = board[x-1][y-1];
@@ -55,18 +55,61 @@ const gameManager = (function() {
     const player1 = prompt('Enter Player 1 name:');
     const player2 = prompt('Enter Player 2 name:');
     let whosTurn = player1;
+    let canPlay = true;
 
     const getPlayerNames = () => `${player1} vs ${player2}`;
     const getWhosTurns = () => whosTurn;
 
-    const playRound = (x, y) => {
-        if (whosTurn === player1) {
-            const filled = gameboard.fillCell(x, y, 'x');
-            if (filled) whosTurn = player2;
-        } else {
-            const filled = gameboard.fillCell(x, y, 'o');
-            if (filled) whosTurn = player1;
+    const checkWinner = (marker) => {
+        // columns and rows
+        for (let i = 1; i < 4; i++) {
+            if (gameboard.getCell(i,1) === marker && gameboard.getCell(i, 1) === gameboard.getCell(i,2) && gameboard.getCell(i,2) === gameboard.getCell(i,3)) {
+                return marker;
+            } else if (gameboard.getCell(1,i) === marker && gameboard.getCell(1, i) === gameboard.getCell(2,i) && gameboard.getCell(3,i) === gameboard.getCell(i,3)) {
+                return marker;
+            }
         }
+
+        // diagnols
+        if (gameboard.getCell(1,1) === marker && gameboard.getCell(1, 1) === gameboard.getCell(2,2) && gameboard.getCell(2,2) === gameboard.getCell(3,3)) {
+            return marker;
+        } else if (gameboard.getCell(1,3) === marker && gameboard.getCell(1, 3) === gameboard.getCell(2,2) && gameboard.getCell(2,2) === gameboard.getCell(3,1)) {
+            return marker;
+        }
+
+        return 'no winner';
+    };
+
+    const playRound = (x, y) => {
+        if (canPlay) {
+            if (whosTurn === player1) {
+                const filled = gameboard.fillCell(x, y, 'x');
+                if (filled) {
+                    const winner = checkWinner('x');
+                    if (winner === 'x') {
+                        canPlay = false;
+                        endGame(player1);
+                    } else {
+                        whosTurn = player2;
+                    }
+                }
+            } else {
+                const filled = gameboard.fillCell(x, y, 'o');
+                if (filled) {
+                    const winner = checkWinner('o');
+                    if (winner === 'o') {
+                        canPlay = false;
+                        endGame(player2);
+                    } else {
+                        whosTurn = player1;
+                    }
+                }
+            }
+        }
+    };
+
+    const endGame = (player) => {
+        console.log(`Congrulations ${player}! You won!`);
     };
 
     return {getPlayerNames, getWhosTurns, playRound};
